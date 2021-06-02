31e7ae991242cd422f4f9674ffa10def2ac40f2d
kecco9939 <kecco9939@gmail.com>
2021-06-02 19:37:24 +0200

    made the login activity, the main activity, modified androidx.room dependencies in build.gradle

--- app/src/main/java/it/skotlinyard/scan4students/LoginActivity.kt
+++
@@ -1,4 +0,0 @@
-package it.skotlinyard.scan4students
-
-class LoginActivity {
-}
--- app/src/main/java/it/skotlinyard/scan4students/MainActivity.kt
+++ app/src/main/java/it/skotlinyard/scan4students/MainActivity.kt
@@ -2,10 +2,15 @@ package it.skotlinyard.scan4students
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import it.skotlinyard.scan4students.databinding.ActivityMainBinding
+import it.skotlinyard.scan4students.databinding.ActivityMainBinding.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
