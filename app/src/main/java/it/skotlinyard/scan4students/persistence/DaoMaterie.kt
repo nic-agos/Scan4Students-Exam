ecd218be30dbff9a3bc7aa6ce81b1586c3531d73
LorenzoAm <72865087+LorenzoAm@users.noreply.github.com>
2021-06-08 21:29:39 +0200

    added dao for Studenti e Materie.

--- app/src/main/java/it/skotlinyard/scan4students/persistence/DaoMaterie.kt
+++ app/src/main/java/it/skotlinyard/scan4students/persistence/DaoMaterie.kt
@@ -1,4 +1,13 @@
 package it.skotlinyard.scan4students.persistence
 
+import androidx.room.Insert
+import androidx.room.Query
+
 interface DaoMaterie {
+
+    @Insert
+    fun insertSubject(subject: Materie)
+
+    @Query("")
+    fun deleteSubject(materia: String)
 }
