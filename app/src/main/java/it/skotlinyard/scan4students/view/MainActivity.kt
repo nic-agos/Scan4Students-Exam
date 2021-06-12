72832ea9838ad6742ff6e1a8ff0132a9239bad60
kecco9939 <kecco9939@gmail.com>
2021-06-12 11:52:17 +0200

    saved icons added

--- app/src/main/java/it/skotlinyard/scan4students/view/MainActivity.kt
+++ app/src/main/java/it/skotlinyard/scan4students/view/MainActivity.kt
@@ -3,12 +3,15 @@ package it.skotlinyard.scan4students.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import it.skotlinyard.scan4students.control.LoginController
 import it.skotlinyard.scan4students.databinding.ActivityMainBinding
 import it.skotlinyard.scan4students.databinding.ActivityMainBinding.*
 //This is the login activity, set as the main activity. Every time the app is launched, users have to login
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    private var controller = LoginController(this)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = inflate(layoutInflater)
@@ -20,8 +23,16 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
         binding.loginBtn.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
+            //get username and psw, send them to the controller to verify
+            val bool = controller.verifyCredentials(binding.email.text.toString(),binding.psw.text.toString())
+
+            if(bool){ //login succesful, go to HomeActivity
+                val intent = Intent(this, HomeActivity::class.java)
+                startActivity(intent)
+            }
+            else{ //login unsuccesful, try again
+                Toast.makeText(this, "Wrong Username or Password", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
