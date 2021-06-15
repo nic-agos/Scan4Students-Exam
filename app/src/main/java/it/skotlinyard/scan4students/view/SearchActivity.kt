e7057d987a9113b861520f5e5f3079f35bad8ad7
kecco9939 <kecco9939@gmail.com>
2021-06-15 17:42:04 +0200

    initialized logic in search activity

--- app/src/main/java/it/skotlinyard/scan4students/view/SearchActivity.kt
+++ app/src/main/java/it/skotlinyard/scan4students/view/SearchActivity.kt
@@ -2,17 +2,22 @@ package it.skotlinyard.scan4students.view
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import it.skotlinyard.scan4students.R
 import it.skotlinyard.scan4students.databinding.ActivityCameraBinding
 import it.skotlinyard.scan4students.databinding.ActivitySearchBinding
 
 class SearchActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivitySearchBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySearchBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
 
         binding.radioNotebook.setOnClickListener {
             binding.facultySpinner.visibility= View.VISIBLE
@@ -21,9 +26,9 @@ class SearchActivity: AppCompatActivity() {
             binding.facultyText2.visibility= View.VISIBLE
             binding.professorText.visibility= View.VISIBLE
             binding.subjectText.visibility= View.VISIBLE
-            binding.searchButton2.visibility=View.VISIBLE
+            binding.searchButtonNote.visibility=View.VISIBLE
             binding.textInputLayout.visibility=View.GONE
-            binding.searchButton.visibility=View.GONE
+            binding.searchButtonUser.visibility=View.GONE
         }
         binding.radioUser.setOnClickListener {
             binding.facultySpinner.visibility= View.GONE
@@ -32,9 +37,56 @@ class SearchActivity: AppCompatActivity() {
             binding.facultyText2.visibility= View.GONE
             binding.professorText.visibility= View.GONE
             binding.subjectText.visibility= View.GONE
-            binding.searchButton2.visibility=View.GONE
+            binding.searchButtonNote.visibility=View.GONE
             binding.textInputLayout.visibility=View.VISIBLE
-            binding.searchButton.visibility=View.VISIBLE
+            binding.searchButtonUser.visibility=View.VISIBLE
+        }
+
+        binding.searchButtonNote.setOnClickListener {
+            if(binding.subjectSpinner.selectedItem.toString().isBlank()&&
+                binding.facultySpinner.selectedItem.toString().isBlank()&&
+                binding.professorSpinner.selectedItem.toString().isBlank())
+                Toast.makeText(this, R.string.search_error , Toast.LENGTH_SHORT).show()
+
+            if(binding.subjectSpinner.selectedItem.toString().isNotBlank()&&
+                binding.facultySpinner.selectedItem.toString().isBlank()&&
+                binding.professorSpinner.selectedItem.toString().isBlank()) {
+
+            }
+            if(binding.subjectSpinner.selectedItem.toString().isBlank()&&
+                binding.facultySpinner.selectedItem.toString().isNotBlank()&&
+                binding.professorSpinner.selectedItem.toString().isBlank()){
+
+            }
+            if(binding.subjectSpinner.selectedItem.toString().isBlank()&&
+                binding.facultySpinner.selectedItem.toString().isBlank()&&
+                binding.professorSpinner.selectedItem.toString().isNotBlank()){
+
+            }
+            if(binding.subjectSpinner.selectedItem.toString().isNotBlank()&&
+                binding.facultySpinner.selectedItem.toString().isNotBlank()&&
+                binding.professorSpinner.selectedItem.toString().isBlank()){
+
+            }
+            if(binding.subjectSpinner.selectedItem.toString().isNotBlank()&&
+                binding.facultySpinner.selectedItem.toString().isBlank()&&
+                binding.professorSpinner.selectedItem.toString().isNotBlank()) {
+
+            }
+            if(binding.subjectSpinner.selectedItem.toString().isBlank()&&
+                binding.facultySpinner.selectedItem.toString().isNotBlank()&&
+                binding.professorSpinner.selectedItem.toString().isNotBlank()) {
+
+            }
+            if(binding.subjectSpinner.selectedItem.toString().isNotBlank()&&
+                binding.facultySpinner.selectedItem.toString().isNotBlank()&&
+                binding.professorSpinner.selectedItem.toString().isNotBlank()){
+
+            }
+        }
+
+        binding.searchButtonUser.setOnClickListener {
+
         }
     }
 }
