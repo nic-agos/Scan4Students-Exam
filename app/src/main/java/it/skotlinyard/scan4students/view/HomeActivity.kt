50ce21875aeb2ce26c3c2c7ace3a99763e1ae562
kecco9939 <kecco9939@gmail.com>
2021-06-20 17:38:50 +0200

    added menu to the home

--- app/src/main/java/it/skotlinyard/scan4students/view/HomeActivity.kt
+++ app/src/main/java/it/skotlinyard/scan4students/view/HomeActivity.kt
@@ -2,14 +2,18 @@ package it.skotlinyard.scan4students.view
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import it.skotlinyard.scan4students.R
 import it.skotlinyard.scan4students.databinding.ActivityHomeBinding
 import it.skotlinyard.scan4students.utils.Session
 
 class HomeActivity: AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+
     override fun onBackPressed() {
         val prev = intent.getStringExtra("prev")
         if(prev=="login"){
@@ -26,7 +30,7 @@ class HomeActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
-        supportActionBar?.hide()
+        supportActionBar?.setTitle(R.string.app_name)
         setContentView(binding.root)
 
         binding.searchButtonH.setOnClickListener{
@@ -45,6 +49,30 @@ class HomeActivity: AppCompatActivity() {
          //   Toast.makeText(this, "work in progress", Toast.LENGTH_LONG).show()
         }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_home, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.about ->
+                Toast.makeText(this, R.string.app_name,
+                    Toast.LENGTH_LONG).show()
+            R.id.settings -> {
+                Toast.makeText(this, "Work in Progress",
+                    Toast.LENGTH_LONG).show()
+            }
+            R.id.logout->{
+                Session.setLogged(false)
+                Session.setCurrUsername("")
+                val intent= Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
 
 }
